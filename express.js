533bc09b213c05c339f62205132768d4295ffe66
Haleigh Dandridge <haleighdandridge@calvisitor-10-105-131-21.calvisitor.1918.berkeley.edu>
2018-05-17 21:24:15 -0700

    server paths

--- express.js
+++ express.js
@@ -1,3 +1,5 @@
+//import { dirname } from "path";
+var path = require("path")
 
  var express = require("express");
  var bodyParser = require("body-parser");
@@ -8,13 +10,23 @@
  
  // Sets an initial port. We"ll use this later in our listener
  var PORT = process.env.PORT || 3000;
+
+//makes the whole app use body parser and convert to JSON 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
  
  //get request/ response
  app.get("/", function(req,res){
     console.log(res)
     res.send("This server works")
  })
- 
+
+ app.get("/tables", function(req,res){
+     res.sendFile(path.join(__dirname + "/tables.html"))
+     console.log(path)
+ })
  
  // LISTENER
  app.listen(PORT, function() {
