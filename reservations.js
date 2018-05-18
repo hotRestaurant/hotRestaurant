c5ff14aba98aec0c44f3c71c1bd0523808a14567
Haleigh Dandridge <haleighdandridge@calvisitor-10-105-131-21.calvisitor.1918.berkeley.edu>
2018-05-17 21:06:29 -0700

    comments

--- reservations.js
+++ reservations.js
@@ -1,10 +1,13 @@
+//Empty array to push all new customers into
 var CustomersArray = []
 
+//variables to capture inputs
 var name = $("#name-input").val().trim()
 var phone =$("#phone-input").val().trim()
 var email = $("#email-input").val().trim()
 var uniqueId = $("#unique-input").val().trim()
 
+//Constructor function to build new customer from input vars
 function CurrentCustomer(name, phone, email, uniqueId) {
    this.name = name;
    this.phone = phone;
@@ -12,6 +15,7 @@ function CurrentCustomer(name, phone, email, uniqueId) {
    this.uniqueId = uniqueId;
 }
 
+//on clicking submit creates a new customer objects and pushes it into array
 $("submit-button").click(function(){
     var CurrentCustomer = new CurrentCustomer(name, phone, email, uniqueId)
     console.log(CurrentCustomer)
